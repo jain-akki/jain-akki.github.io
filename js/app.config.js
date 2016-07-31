f179840f55006a1340e11695b907bef96bc45395
Akshay Jain <jain.akshay5244@gmail.com>
2016-07-31 17:10:04 +0530

    Add Toggle Menu Function in mainCtrl
    Display more content in gitProfile View

--- js/app.config.js
+++ js/app.config.js
@@ -9,15 +9,32 @@
     $stateProvider
      .state('main', {
        url: '/',
-       abstract: true, 
+       abstract: true,
        templateUrl: 'index.html',
        controller: 'mainCtrl as vm'
      })
     .state('gitProfile', {
       url: '/gitProfile',
-      templateUrl: 'templates/mainContent.html',
+      templateUrl: 'templates/gitProfile.html',
       controller: 'githubDetailsCtrl as vm'
+    })
+    .state('aboutMe', {
+      url: '/aboutMe',
+      templateUrl: 'templates/aboutMe.html',
+      controller: 'aboutCtrl as vm'
     });
+
     $urlRouterProvider.otherwise('/gitProfile');
+
+  })
+  .config(function ($mdThemingProvider, $mdIconProvider) {
+
+    $mdIconProvider
+      .icon("menu", "./img/svg/menu.svg", 24);
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('grey')
+      .accentPalette('red');
+
   });
 })();
--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -4,25 +4,37 @@
 
   angular.module('githubPortfolio')
 
-     .controller('mainCtrl', function () {
+     .controller('mainCtrl', function ($mdSidenav, userData, $rootScope) {
        var vm = this;
+
        console.log('mainContent');
+
+       vm.sideMenuItem = [
+         { id: 1, name: 'Github Portfolio', state: 'gitProfile' },
+         {id: 2, name: 'About Me',  state: 'aboutMe'}
+       ];
+
+       userData.getUserInfo('jain-akki').then(function (response) {
+         $rootScope.allInfo = response;
+         console.log('allInfo', $rootScope.allInfo);
+       });
+
+       vm.toggle = function () {
+         console.log('Btn Clicked!!');
+         $mdSidenav('left').toggle();
+       }
+
      })
-    .controller('githubDetailsCtrl', function (userData, $mdSidenav) {
+    .controller('githubDetailsCtrl', function (userData) {
 
       var vm = this;
 
-      console.log('githubProfileCtrl');
-
-      userData.getUserInfo('jain-akki').then(function (response) {
-        vm.allInfo = response;
-        console.log('vm.allInfo', vm.allInfo);
-      });
-
-      vm.toggle = function () {
-        $mdSidenav('left').toggle();
-      }
+      console.log('githubProfileCtrl'); 
 
     })
+    .controller('aboutCtrl', function () {
+      var vm = this;
+      console.log('aboutCtrl');
+    });
    
 })();
